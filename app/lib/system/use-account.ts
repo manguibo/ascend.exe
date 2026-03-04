d1a2bebfd00cc65b7db3d54eeae54e10ecf119b2
Benjamin Manguiat <benmango17@gmail.com>
2026-03-03 21:53:55 -0500

    fix(account): stabilize server snapshot in useAccount hook

--- app/lib/system/use-account.ts
+++ app/lib/system/use-account.ts
@@ -3,9 +3,10 @@
 import { useSyncExternalStore } from "react";
 import { getAccounts, getCurrentAccount, subscribeAccountState } from "./account-state";
 
+const emptyAccounts: readonly ReturnType<typeof getAccounts>[number][] = [];
+
 export function useAccount() {
   const currentAccount = useSyncExternalStore(subscribeAccountState, getCurrentAccount, () => null);
-  const accounts = useSyncExternalStore(subscribeAccountState, getAccounts, () => []);
+  const accounts = useSyncExternalStore(subscribeAccountState, getAccounts, () => emptyAccounts);
   return { currentAccount, accounts };
 }
-
